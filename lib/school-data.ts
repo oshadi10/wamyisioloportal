d352bb046851f02c872c4d74c9e05c982f2b9a97
oshadi10 <oshadi10@gmail.com>
2026-08-30 10:10:36 +0300

    Update school-data.ts

--- lib/school-data.ts
+++ lib/school-data.ts
@@ -35,7 +35,7 @@ export interface Lecturer {
 
 export const lecturers: Lecturer[] = [
   { name: 'Mr. Osman Halake', subject: 'Mathematics / Business Studies', email: 'osman@wamyisiolo.sc.ke', password: 'osman5515', isAdmin: true },
-  { name: 'Mr. Osman Halake', subject: 'Administration', email: 'manager@wamyisiolcenter.local', password: 'Manager123!', isAdmin: true },
+  { name: 'Mr. Abdullahi Sama', subject: 'Administration', email: 'manager@wamyisiolcenter.local', password: 'Manager123!', isAdmin: true },
   { name: 'Mr. Guyo Halake', subject: 'Arabic / Islamic Religious Education', email: 'guyo@wamyisiolo.sc.ke', password: 'guyo111' },
   { name: 'Mr. Dennis Kipkoech', subject: 'Mathematics / Physics', email: 'dennis@wamyisiolo.sc.ke', password: 'dennis222' },
   { name: 'Mr. John Simiyu', subject: 'Kiswahili / History', email: 'john@wamyisiolo.sc.ke', password: 'john373' },
